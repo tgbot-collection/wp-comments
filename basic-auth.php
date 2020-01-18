5897a5b99eb585475f4a92bc07f892dd9a583eed
Benny~ <benny.think@gmail.com>
2020-01-18 20:20:20 +0800

    remove redundant settings from wp baisc auth

--- basic-auth.php
+++ basic-auth.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * Plugin Name: JSON Basic Authentication
- * Description: Basic Authentication handler for the JSON API, used for development and debugging purposes
- * Author: WordPress API Team
- * Author URI: https://github.com/WP-API
- * Version: 0.1
- * Plugin URI: https://github.com/WP-API/Basic-Auth
- */
 
 function json_basic_auth_handler( $user ) {
 	global $wp_json_basic_auth_error;
